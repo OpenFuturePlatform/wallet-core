a8c7911027d155294343abd27632e79c33a07bfb
borbuevbeksultan <beksultan@openfuture.io>
2022-11-21 20:29:09 +0600

    SA-82 Encryption and decryption

---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/beksultan/IdeaProjects/wallet-core/tests
+# Build directory: /Users/beksultan/IdeaProjects/wallet-core/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example_test "/Users/beksultan/IdeaProjects/wallet-core/tests/tests")
+set_tests_properties(example_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/beksultan/IdeaProjects/wallet-core/tests/CMakeLists.txt;51;add_test;/Users/beksultan/IdeaProjects/wallet-core/tests/CMakeLists.txt;0;")
+subdirs("googletest-build")
---
+++ walletconsole/CMakeFiles/walletconsole.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/beksultan/IdeaProjects/wallet-core/walletconsole/main.cpp" "walletconsole/CMakeFiles/walletconsole.dir/main.cpp.o" "gcc" "walletconsole/CMakeFiles/walletconsole.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/beksultan/IdeaProjects/wallet-core/walletconsole/lib/CMakeFiles/walletconsolelib.dir/DependInfo.cmake"
+  "/Users/beksultan/IdeaProjects/wallet-core/trezor-crypto/CMakeFiles/TrezorCrypto.dir/DependInfo.cmake"
+  "/Users/beksultan/IdeaProjects/wallet-core/CMakeFiles/TrustWalletCore.dir/DependInfo.cmake"
+  "/Users/beksultan/IdeaProjects/wallet-core/CMakeFiles/protobuf.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
